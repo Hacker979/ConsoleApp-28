4adb20251e4e655a694e3b8bd41156a56d79bfb8
Kab-35-16 <Kab-35-16@KAB-31-11->
2025-03-05 09:24:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void PrintMas(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] % 2 == 0)
+                {
+                    Console.Write(mas[i] + " ");
+                }
+            }
+        }
+        static void Mas()
+        {
+            int[] mas = new int[14];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"mas[{i}] = ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            PrintMas(mas);
+        }
+
+        static void Main(string[] args)
+        {
+            Mas();
+
+            Console.Read();
+        }
+    }
+}
